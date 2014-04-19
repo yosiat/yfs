5629c6943d5ad5f5528683c102fb52fda37e801f
Yosi Attias <yosy101@gmail.com>
2014-04-20 01:53:53 +0300

    - Adding setup.py - py.test! - Requirements!

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='yfs',
+    version='',
+    packages=['yfs', 'yfs.Protocol', 'yfs.chunkserver', 'yfs.masterserver'],
+    url='',
+    license='',
+    author='Yosi',
+    author_email='',
+    description=''
+)
